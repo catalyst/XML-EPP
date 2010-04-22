7ab2848e012c022845bb6677d70b6f05565be054
Andrew Chilton <andychilton@gmail.com>
2010-04-22 14:11:13 +1200

    rfc5731 - slight tidy up of Domain.pm

--- lib/XML/EPP/Domain.pm
+++ lib/XML/EPP/Domain.pm
@@ -12,14 +12,15 @@ BEGIN {
 	enum "XML::EPP::Domain::hostsType" => qw( all del none sub );
 }
 
-#use XML::EPP::Domain::Create;
 use XML::EPP::Domain::Check;
-#use XML::EPP::Domain::Delete;
 use XML::EPP::Domain::Info;
+
+#use XML::EPP::Domain::Create;
+#use XML::EPP::Domain::Delete;
 #use XML::EPP::Domain::Update;
 
-#use XML::EPP::Domain::Check::Response;
-#use XML::EPP::Domain::Info::Response;
+use XML::EPP::Domain::Check::Response;
+use XML::EPP::Domain::Info::Response;
 #use XML::EPP::Domain::Create::Response;
 
 #use XML::EPP::Domain::Notification;
