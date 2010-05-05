5cb6541dfff45e03f6197f7f2304c23985d97bf2
Andrew Chilton <andy@catalyst.net.nz>
2010-05-05 12:41:47 +1200

    rfc-5733 - add the Delete command

--- lib/XML/EPP/Contact.pm
+++ lib/XML/EPP/Contact.pm
@@ -62,6 +62,7 @@ use XML::EPP::Contact::Check;
 use XML::EPP::Contact::Info;
 use XML::EPP::Contact::Transfer;
 use XML::EPP::Contact::Create;
+use XML::EPP::Contact::Delete;
 
 use XML::EPP::Contact::Check::Response;
 use XML::EPP::Contact::Info::Response;
