63cd51914d17fc7addf6d36661bab705a3075913
Sam Crawley <samc@catalyst.net.nz>
2010-09-03 16:11:55 +1200

    wr72631: added max sig life to create domain

--- lib/XML/EPP/DNSSEC/Create.pm
+++ lib/XML/EPP/DNSSEC/Create.pm
@@ -7,6 +7,13 @@ sub root_element {'create'}
 
 with 'XML::EPP::DNSSEC', 'XML::EPP::DNSSEC::Node';
 
+has_element 'max_sig_life' =>
+	is => 'ro',
+	isa => 'Int',
+	xml_required => 0,
+	xml_nodeName => 'maxSigLife',
+	;
+
 with 'XML::EPP::DNSSEC::Elements';
 
 1;
